7b8585e9df7fc05e93d43999ae608b29fb00825c
khalidch73 <khalidnawazch73@gmail.com>
2023-12-03 23:42:59 +0500

    update

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,17 +1,45 @@
-import React from 'react';
+// components/Navbar.tsx
 import Link from 'next/link';
+import Image from 'next/image';
+import Logo from '@/app/images/logo.png'; // Replace with your actual logo image
+import React from 'react';
+
 const Navbar = () => {
   return (
-    <nav className="bg-gray-800 text-white p-4 px-10 flex justify-between items-center fixed w-full">
-      <div>
-        <span className="text-xl font-bold">Khalid Nawaz</span>
-      </div>
-      <div>
-        <button className="bg-gradient-to-r from-red-900 to-yellow-300 text-white font-bold py-2 px-4 rounded-full">
-         <Link href={'https://www.linkedin.com/in/khalid-nawaz-bost-your-business-with-guest-posting-33b7661aa/'}>Let's Connect</Link>
-        </button>
-      </div>
-    </nav>
+    <div className="bg-gray-400 text-white py-4 fixed w-full z-10">
+      <nav>
+        <div className="container mx-auto flex justify-around items-center">
+          <Link href="/">
+            <div className="flex items-center">
+              <Image src={Logo} alt="Logo" width={100} height={100} className="mr-2 rounded-lg" />
+              <span className="text-xl font-bold">ModWebSolution</span>
+            </div>
+          </Link>
+          <div className="lg:flex lg:items-center lg:w-auto">
+            <div className="text-lg lg:flex-grow">
+              <Link href="/">
+                <span className="block mt-4 lg:inline-block lg:mt-0 mr-4">Home</span>
+              </Link>
+              <Link href="/about">
+                <span className="block mt-4 lg:inline-block lg:mt-0 mr-4">About</span>
+              </Link>
+              <Link href="/services">
+                <span className="block mt-4 lg:inline-block lg:mt-0 mr-4">Services</span>
+              </Link>
+              <Link href="/portfolio">
+                <span className="block mt-4 lg:inline-block lg:mt-0 mr-4">Portfolio</span>
+              </Link>
+              <Link href="/technologies">
+                <span className="block mt-4 lg:inline-block lg:mt-0 mr-4">Technologies</span>
+              </Link>
+              <Link href="/contact">
+                <span className="block mt-4 lg:inline-block lg:mt-0">Contact</span>
+              </Link>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </div>
   );
 };
 
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,10 +7,8 @@ const config: Config = {
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+        'hero-section': "url('/app/images/nb-background-image.jpg')",
+      }
     },
   },
   plugins: [],
